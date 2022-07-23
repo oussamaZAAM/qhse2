ba7fda904d1d8773ff51af5b5bbfc883fe5f0b8d
Ayoubon <aarrasseayoub@gmail.com>
2022-07-23 16:45:51 +0100

    ayoub

--- Rooms-oussama/src/App.js
+++ Rooms-oussama/src/App.js
@@ -1,15 +1,21 @@
-import React, { useContext } from "react"
+import React, { useContext, useParams } from "react"
 import Login from "./components/Login"
 import Register from "./components/Register"
 import Navbar from "./components/Navbar"
-import Organism from "./components/NewOrganism"
+import NewOrganism from "./components/NewOrganism"
+import Organism from "./components/Organism"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthContext } from "./Context/authContext";
 
 
 export default function App() {
   const {user} = useContext(AuthContext);
-
+  function HandleOrganism() {
+    let { id } = useParams();
+    return (
+      <Organism orgId={id} /> 
+    )
+  }
 
     return(
     <BrowserRouter>
@@ -18,7 +24,8 @@ export default function App() {
           <Route path="/login" element={user?<Navbar />:<Login />} />
           <Route path="/register" element={user?<Navbar />:<Register />} />
           <Route path="/" element={<Login />} />
-          <Route path="/new-organism" element={<Organism />} />
+          <Route path="/new-organism" element={<NewOrganism />} />
+          <Route path="/organism/:id" element={<HandleOrganism />} />
         {/* </Route> */}
       </Routes>
     </BrowserRouter>
