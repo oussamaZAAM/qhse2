e91439bd50e2901dcfd815a58eeec6e6bb7a9220
Ayoubon <aarrasseayoub@gmail.com>
2022-07-24 15:38:05 +0100

    ayoub

--- Rooms-oussama/src/App.js
+++ Rooms-oussama/src/App.js
@@ -10,7 +10,7 @@ import OrganismPage from "./screens/OrganismPage/OrganismPage";
 
 
 export default function App() {
-  const {user} = useContext(AuthContext);
+  const {user, org} = useContext(AuthContext);
   function HandleOrganism() {
     let {id} = useParams();
     return (
@@ -20,17 +20,14 @@ export default function App() {
 
     return(
     <BrowserRouter>
-    <div style={{backgroundColor:"#F5F0BB"}}>
-    <img style={{maxHeight:"70px", marginTop:"20px", marginLeft:"20px"}} src="https://bit.ly/3b1DwND" />
-    </div>
+      <Navbar />
       <Routes>
         {/* <Route path="/" element={<Layout />}> */}
-          <Route path="/login" element={user?<OrganismPage />:<Login />} />
-          <Route path="/register" element={user?<OrganismPage />:<Register />} />
-          <Route path="/main" element={<OrganismPage />} />
-          <Route path="/new-organism" element={<NewOrganism />} />
-          <Route path="/organism/:id" element={<HandleOrganism />} />
-          <Route path="/main" element={<OrganismPage />} />
+          <Route path="/login" element={org?<Organism />:(user?<OrganismPage />:<Login />)} />
+          <Route path="/register" element={org?<Organism />:(user?<OrganismPage />:<Register />)} />
+          <Route path="/main" element={org?<Organism />:(user?<OrganismPage />:<Login />)} />
+          <Route path="/new-organism" element={org?<Organism />:(user?<NewOrganism />:<Login />)} />
+          <Route path="/organism/:id" element={org?<Organism />:(user?<HandleOrganism />:<Login />)} />
         {/* </Route> */}
       </Routes>
     </BrowserRouter>
--- Rooms-oussama/src/components/Navbar/Navbar.js
+++ Rooms-oussama/src/components/Navbar/Navbar.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useRef, useContext } from "react";
+import { AuthContext } from "../../Context/authContext";
+import {Link, useNavigate} from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.css';
+import {Button} from 'react-bootstrap';
 
 export default function Navbar() {
+    const { user, dispatch } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        dispatch({ type: "LOGIN_SUCCESS", payload: undefined});
+        localStorage.removeItem("user");
+        navigate("./login")
+      }
+    function handleFinish() {
+        dispatch({ type: "SELECT_SUCCESS", payload: undefined});
+        localStorage.removeItem("orgs");
+        navigate("./main")
+      }
     return(
-        <div className="navbar">
-            <ul className="navbar-list">
-                <li className="navbar-li">
-                    <img className="navbar-logo" src="" />
-                </li>
-                <li className="navbar-li">
-                    <input className="navbar-search" placeholder="Search Rooms..." />
-                </li>
-                <li className="navbar-li">
-                    <div>
-                        <></>
-                        <></>
-                    </div>
-                </li>
-                <li className="navbar-li">
-                    <div className="navbar-user">
-                        <img className="profileimage" src="" />
-                        <p>Username</p>
-                    </div>
-                </li>
-            </ul>
+        <div style={{backgroundColor:"#F5F0BB"}}>
+
+
+        <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+  <a class="navbar-brand" href="../main"><img src="https://bit.ly/3b1DwND" style={{maxHeight:"70px", marginTop:"20px", marginLeft:"20px", paddingBottom:"20px"}}/></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    {user!==undefined && 
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a className="nav-link sortable" onClick={handleFinish}>Terminer</a>
+      </li>
+      <li class="nav-item">
+        <a className="nav-link sortable" onClick={handleLogout}>Deconnecter</a>
+      </li>
+    </ul>
+    }
+  
+</nav>
         </div>
+
     )
 }
