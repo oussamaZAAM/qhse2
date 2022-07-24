d68eb60cfd836dce6cce6fda0d224c4632361e49
Ayoubon <aarrasseayoub@gmail.com>
2022-07-24 15:59:25 +0100

    ayoub

--- Rooms-oussama/src/components/Navbar/Navbar.js
+++ Rooms-oussama/src/components/Navbar/Navbar.js
@@ -5,17 +5,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Button} from 'react-bootstrap';
 
 export default function Navbar() {
-    const { user, dispatch } = useContext(AuthContext);
+    const { user,org, dispatch } = useContext(AuthContext);
     const navigate = useNavigate();
 
     function handleLogout() {
-        dispatch({ type: "LOGIN_SUCCESS", payload: undefined});
+        dispatch({ type: "LOGIN_SUCCESS", payload: [undefined,undefined]});
         localStorage.removeItem("user");
+        localStorage.removeItem("org");
         navigate("./login")
       }
+      console.log(org)
     function handleFinish() {
-        dispatch({ type: "SELECT_SUCCESS", payload: undefined});
-        localStorage.removeItem("orgs");
+        dispatch({ type: "SELECT_SUCCESS", payload: [user,undefined]});
+        localStorage.removeItem("org");
         navigate("./main")
       }
     return(
@@ -29,9 +31,11 @@ export default function Navbar() {
   </button>
     {user!==undefined && 
     <ul class="navbar-nav">
+        {org!==undefined &&
       <li class="nav-item">
         <a className="nav-link sortable" onClick={handleFinish}>Terminer</a>
       </li>
+      }
       <li class="nav-item">
         <a className="nav-link sortable" onClick={handleLogout}>Deconnecter</a>
       </li>
